ec861a4e69bb5d1f87b54fe6ff206c25dc71c71d
nechn <rufflerock@gmail.com>
2023-04-07 12:04:02 +0200

    enable versioning

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,7 +19,7 @@ resource "aws_s3_object" "input_object" {
   for_each = {for input_object in local.input_jsons : input_object.name => input_object}
 
   bucket = aws_s3_bucket.input_bucket.bucket
-  key    = format("subfolder/%s", each.value.name)
+  key    = format("input/%s", each.value.name)
   source = format("../json/%s", each.value.name)
 }
 
@@ -29,5 +29,28 @@ resource "aws_s3_object" "source_object" {
   bucket = aws_s3_bucket.source_bucket.bucket
   key    = format("source/%s", each.value.name)
   source = format("../json/%s", each.value.name)
+}
+
+resource "aws_s3_bucket_inventory" "input_bucket_inventory" {
+  bucket = aws_s3_bucket.input_bucket.id
+  name   = "input_json_daily_inventory"
+
+  included_object_versions = "Current"
+
+  schedule {
+    frequency = "Daily"
+  }
+
+  filter {
+    prefix = "input/"
+  }
+
+  destination {
+    bucket {
+      format     = "CSV"
+      bucket_arn = aws_s3_bucket.input_bucket.arn
+      prefix     = "inventory"
+    }
+  }
 }
 
